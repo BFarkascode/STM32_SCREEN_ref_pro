d237e065584d2a3650e8e239622af379ef6549c1
Balazs Farkas <94067165+BFarkascode@users.noreply.github.com>
2024-06-20 14:26:15 +0200

    Update ili9341.c

--- ili9341.c
+++ ili9341.c
@@ -1,6 +1,9 @@
 /*
  * ili9341.c
  *
+ *  Modified by: Balazs Farkas - 20/06/2024
+ *	Changed all SPI references to SPI5 from SPI1	
+ *
  *  Created on: 2019/12/26
  *      Author: Kotetsu Yamamoto
  *      Copyright [Kotetsu Yamamoto]
